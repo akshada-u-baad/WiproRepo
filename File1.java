044d6ac1f430ac77660376ff08051b8ac76c6e01
Akshada Baad <akshadabaad7878@gmail.com>
2024-05-16 16:46:13 +0530

    Update File1.java added 2nd var
    var 2 =20

--- File1.java
+++ File1.java
@@ -7,6 +7,7 @@ public class File1
 public static void main(String[] args){
                                        
   int a =10;
+  int b = 20;
    System.out.println("First Java1");
    System.out.println("My Second Line code");
                                       }
